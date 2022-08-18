86278830fc7b7a42f3f8b84f486e8632008ad1f4
Nathan Fixler <nathan@fixler.org>
2022-08-18 16:09:32 -0700

    Use Fly's remote builder instance
    This commit adds the `--remote-only` deploy flag. With this flag, the
    docker image will be built on Fly's remote builder instance, where it
    can take advantage of layer caching.

--- entrypoint.sh
+++ entrypoint.sh
@@ -43,9 +43,9 @@ if ! flyctl status --app "$app"; then
     echo "Importing secrets..."
     echo $secrets | tr " " "\n" | flyctl secrets import --app "$app"
   fi
-  flyctl deploy --config "$config" --app "$app" --region "$region" --image "$image" --region "$region" --strategy immediate
+  flyctl deploy --remote-only --config "$config" --app "$app" --region "$region" --image "$image" --region "$region" --strategy immediate
 elif [ "$INPUT_UPDATE" != "false" ]; then
-  flyctl deploy --config "$config" --app "$app" --region "$region" --image "$image" --region "$region" --strategy immediate
+  flyctl deploy --remote-only --config "$config" --app "$app" --region "$region" --image "$image" --region "$region" --strategy immediate
 fi
 
 # Attach postgres cluster to the app if specified.
